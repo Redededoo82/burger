1324d84f1f9cb3018c6c48727abed6317e20042b
AARON M <asmarcks@gmail.com>
2019-10-16 21:43:32 -0500

    finished controller...get/post/put routers completed

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -11,8 +11,29 @@ router.get('/', function(req, res){
         console.log(hdbrsObj);
         res.render('index', hdbrsObj);
     });
+
+
+    router.post('/api/burgers', function(req, res){
+        burger.insertOne(
+            ['burger_name', 'devoured'],
+            [req.body.burger_name, req.body.devoured],
+            function(result){
+                res.json({id: result.insertID});
+            }
+        );
+    });
 });
 
+router.put('/api/burgers/:id', function(req, res){
+    var condition = 'id =' + req.params.id;
 
-
+    console.log('condition', condition);
+    burger.updateOne({ devoured: req.body.devoured}, condition, function(result){
+        if ((result,changedRows === 0)) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 module.exports = router;
