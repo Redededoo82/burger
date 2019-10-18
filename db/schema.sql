78d8c9ab89f07082f37ece2f33bf57b2ba6ec0ef
AARON M <asmarcks@gmail.com>
2019-10-17 22:46:31 -0500

    schema NOT updated ;)

--- db/schema.sql
+++ db/schema.sql
@@ -2,6 +2,8 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
+
+
 CREATE TABLE burgers(
     id INTEGER NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(255) NOT NULL,
